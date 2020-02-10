17ad17384dacc836cc897086237d40b3798cddc5
Tyler Funk <tyler.j.funk2@gmail.com>
2020-02-10 21:52:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,7 +57,9 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.size do
-    return false if source_array[i] == false
+    if source_array[i] == false
+      return false
+    end
     i += 1
   end
   return true
