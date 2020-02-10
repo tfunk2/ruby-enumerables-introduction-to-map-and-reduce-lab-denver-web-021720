bd71728e470efc7bd419d35d7440a91c730065e0
Tyler Funk <tyler.j.funk2@gmail.com>
2020-02-10 21:36:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,72 @@
-# My Code here....
+def map_to_negativize(source_array)
+  new_array = []
+  i = 0 
+  while i < source_array.length do
+    new_array << (source_array[i] * -1)
+    i += 1
+  end
+  new_array
+end
+
+
+def map_to_no_change(source_array)
+  new_array = []
+  i = 0 
+  while i < source_array.length do
+    new_array << source_array[i]
+    i += 1
+  end
+  new_array
+end
+
+
+def map_to_double(source_array)
+  new_array = []
+  i = 0 
+  while i < source_array.length do
+    new_array << (source_array[i] * 2)
+    i += 1
+  end
+  new_array
+end
+
+
+def map_to_square(source_array)
+  new_array = []
+  i = 0 
+  while i < source_array.length do
+    new_array << (source_array[i] ** 2)
+    i += 1
+  end
+  new_array
+end
+
+
+def reduce_to_total(source_array, starting_point = 0)
+  total = starting_point
+  i = 0 
+  
+  while i < source_array.size do
+    total += source_array[i]
+    i += 1
+  end
+  total
+end
+
+
+def reduce_to_all_true(source_array)
+  i = 0 
+  while i < source_array.size do
+    if source_array[i] != true
+      false
+    else
+      i += 1
+    end
+  end
+  true
+end
+
+
+def reduce_to_any_true(source_array)
+end
+
